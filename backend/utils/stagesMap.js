684df932e298ebaa479bbcbde5303aedefcbf80c
Elias Garzon <eliasgarzon@gmail.com>
2022-01-12 11:09:08 +0200

    backend boiler plate

---
+++ backend/utils/stagesMap.js
@@ -0,0 +1,10 @@
+export default {
+  0: "available",
+  1: "adopted/fostered pending approval",
+  2: "adopted",
+  3: "fostered",
+  4: "return pending approval",
+  5: "return approved",
+  6: "convert foster to adoption pending approval",
+  7: "approve foster to adoption",
+};
