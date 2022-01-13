02ba1962c5559537827c3c8f2402a5d66b7bdb2c
Haim Shragai <haimke11@gmail.com>
2022-01-13 14:18:03 +0200

    fixed order of api arguments

--- data science/api/hackaton_api.py
+++ data science/api/hackaton_api.py
@@ -15,12 +15,12 @@ def predict_price():
     features_dic = {'pickup_day': request.args.get('pickup_day'),
                     'pickup_hour': request.args.get('pickup_hour'),
                     'pickup_minute': request.args.get('pickup_minute'),
+                    'passenger_count': request.args.get('passenger_count'),
+                    'trip_distance': request.args.get('trip_distance'),
                     'pickup_longitude': request.args.get('pickup_longitude'),
                     'pickup_latitude': request.args.get('pickup_latitude'),
-                    'trip_distance': request.args.get('trip_distance'),
                     'dropoff_longitude': request.args.get('dropoff_longitude'),
                     'dropoff_latitude': request.args.get('dropoff_latitude'),
-                    'passenger_count': request.args.get('passenger_count')
                     }
 
     X_new = np.fromiter(features_dic.values(), dtype=float)
