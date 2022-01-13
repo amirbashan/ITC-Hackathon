b08d7ee7ac514c9e843640119b27b0899c48842b
Amir Bashan <90416342+amirbashan@users.noreply.github.com>
2022-01-13 15:06:11 +0200

    date added to search

--- frontend/src/pages/ResultsPage/ResultsPage.jsx
+++ frontend/src/pages/ResultsPage/ResultsPage.jsx
@@ -1,63 +1,60 @@
-import React,{useEffect,useState,} from 'react'
-import { useLocation,useParams } from 'react-router-dom';
-import './ResultsPage.css';
-import RideTile from '../../components/RideTile/RideTile';
-import Button from '../../components/Button/Button';
-import axios from 'axios' 
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import "./ResultsPage.css";
+import RideTile from "../../components/RideTile/RideTile";
+import Button from "../../components/Button/Button";
+import axios from "axios";
 const ResultsPage = () => {
+  const queryString = useLocation().search;
+  const params = queryString.split("&").map((param) => param.split("="));
 
-const queryString = useLocation().search
-const params=queryString.split("&").map((param)=>param.split("="))
+  // const {lat}=useParams();
+  console.log(params);
 
-// const {lat}=useParams();
-console.log(params);
+  const lat = params[0][1];
+  const lng = params[1][1];
+  const latEnd = params[2][1];
+  const lngEnd = params[3][1];
+  const [results, setResults] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useState();
+  async function getResults() {
+    const radius = 1;
+    try {
+      setLoading(true);
+      const data = await axios.get(
+        `https://ride2gether-api.herokuapp.com/api/v1/rides/rides-within/${radius}/center/${lat},${lng}/end/${latEnd},${lngEnd}/unit/km`
+      );
+      setResults(data.data);
+      console.log(results);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  }
 
-const lat = params[0][1];
-const lng=params[1][1];
+  useEffect(() => {
+    getResults();
+  }, []);
 
-    const [results, setResults] = useState(null)
-    const [loading, setLoading] = useState(false)
-    useState()
-     async function getResults (radius,lat,lng)
-     {
-         radius=1
-         try{
-             setLoading(true)
-        const data= await axios.get(`https://ride2gether-api.herokuapp.com/api/v1/rides/rides-within/${radius}/center/${lat},${lng}/unit/km`)
-        setResults( data.data)
-              console.log(results);
-
-         }
-         catch(e){
-             console.log(e)
-         }
-         finally{
-             setLoading(false)
-         }
-        }
-    
-    
-    useEffect(() => {
-      getResults(1,lat,lng)
-        
-    }, [])
-
-    return (
-      <>
-        <div id="resultsPage">
-          <div id="resultsList">
-              {loading && "Loading"}
-            {results && (results.length>0) && results.map((ride) => {
+  return (
+    <>
+      <div id="resultsPage">
+        <div id="resultsList">
+          {loading && "Loading"}
+          {results &&
+            results.length > 0 &&
+            results.map((ride) => {
               return <RideTile key={ride._id} name={ride.createdBy} />;
             })}
-          </div>
-          
         </div>
-        <div className="cancel-container">
-          <Button isGrow={true} text="Cancel" />
-        </div>
-      </>
-    );
-}
+      </div>
+      <div className="cancel-container">
+        <Button isGrow={true} text="Cancel" />
+      </div>
+    </>
+  );
+};
 
-export default ResultsPage
+export default ResultsPage;
